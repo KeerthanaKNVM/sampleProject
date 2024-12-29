86ae728dc48f2cc527a4447f5c33dc3c3dfd9a1f
Keerthana Kesanakurthi <keerthiknvm@gmail.com>
2024-12-29 12:14:23 +0530

    final commit

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Keerthi is mental");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Keerthi is mental");
+        }
     }
 }
